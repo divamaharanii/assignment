bdafc0ad72d1ffdf850d66c5f26e8f8b79416fe8
divamaharanii <divamaharaniip@gmail.com>
2022-12-21 22:55:14 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Diva Maharani Putrihedi" // TODO: replace this
+const studentId = "FE3291491" // TODO: replace this
+
+export { url, studentName, studentId }
